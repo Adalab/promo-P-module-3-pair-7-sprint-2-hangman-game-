b0b5f2ac4a9fe2d23016aeb80bc97f788dbd413e
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-04-27 10:29:02 +0200

    migrate solution sprint 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,91 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+// import { Route, Switch } from 'react-router-dom';
+
+// api
+import getWordFromApi from '../services/api';
+// styles
 import '../styles/App.scss';
 
 function App() {
-  const [numberOfErrors, setOfError] = useState(0);
-  const [lastLetter, setlastLetter] = useState('');
+  const [word, setWord] = useState('');
+  const [userLetters, setUserLetters] = useState([]);
+  const [lastLetter, setLastLetter] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  const maxNumberOfErrors = 13;
 
-  const handlerError = (event) => {
-    event.preventDefault();
-    setOfError(numberOfErrors + 1);
+  useEffect(() => {
+    setLoading(true);
+    getWordFromApi().then((word) => {
+      setWord(word);
+      setLoading(false);
+    });
+  }, []);
+
+  // events
+
+  const handleWord = (value) => {
+    setWord(value);
+    setUserLetters([]);
+    setLastLetter('');
   };
 
-  const hadleInputLetter = (event) => {
-    event.preventDefault();
-    const newLetter = event.target.value;
-    // Creamos una constante con la expresion regular que queremos utilizar
-    const regular = /[a-zA-Z]/;
-    // Comprobamos que la letra que obtenemos es válida según nuestra expresión regular (guardada en la constante 'regular')
-    if(newLetter === '' || regular.test(newLetter)) { // Si es válida...
-       // ... guardamos el valor 'lastaLetter'
-       setlastLetter(newLetter);
+  const handleKeyDown = (ev) => {
+    // Sabrías decir para qué es esta línea
+    ev.target.setSelectionRange(0, 1);
+  };
 
-       console.log(newLetter);
-    } // Sino no hagas nada
-    
+  const handleChange = (ev) => {
+    let re = /[a-zA-Z]/; //add regular pattern - lesson 3.3 exercise 2
+    if (re.test(ev.target.value)) {
+      handleLastLetter(ev.target.value);
+    }
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
+
+  const getNumberOfErrors = () => {
+    const errorLetters = userLetters.filter(
+      (letter) => word.includes(letter) === false
+    );
+    return errorLetters.length;
+  };
+
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split('');
+    return wordLetters.map((letter, index) => {
+      const exists = userLetters.includes(letter.toLocaleLowerCase());
+      return (
+        <li key={index} className="letter">
+          {exists ? letter : ''}
+        </li>
+      );
+    });
+  };
+
+  const renderErrorLetters = () => {
+    const errorLetters = userLetters.filter(
+      (letter) =>
+        word.toLocaleLowerCase().includes(letter.toLocaleLowerCase()) === false
+    );
+    return errorLetters.map((letter, index) => {
+      return (
+        <li key={index} className="letter">
+          {letter}
+        </li>
+      );
+    });
+  };
+
+  const handleLastLetter = (value) => {
+    value = value.toLocaleLowerCase();
+    setLastLetter(value);
+    if (!userLetters.includes(value)) {
+      userLetters.push(value);
+      setUserLetters([...userLetters]);
+    }
   };
 
   return (
@@ -34,61 +97,44 @@ function App() {
         <section>
           <div className="solution">
             <h2 className="title">Solución:</h2>
-            <ul className="letters">
-              <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li>
-            </ul>
+            <ul className="letters">{renderSolutionLetters()}</ul>
           </div>
           <div className="error">
             <h2 className="title">Letras falladas:</h2>
-            <ul className="letters">
-              <li className="letter">f</li>
-              <li className="letter">q</li>
-              <li className="letter">h</li>
-              <li className="letter">p</li>
-              <li className="letter">x</li>
-            </ul>
+            <ul className="letters">{renderErrorLetters()}</ul>
           </div>
-          <form className="form">
+          <form className="form" onSubmit={handleSubmit}>
             <label className="title" htmlFor="last-letter">
               Escribe una letra:
             </label>
             <input
+              autoFocus
               autoComplete="off"
               className="form__input"
               maxLength="1"
               type="text"
               name="last-letter"
               id="last-letter"
-              /* value = {lastLetter.replace(/[.,;:_*+\-=¿?¡!^'`${}()<>|\/[\]\\]/g,'')} */
-              value = {lastLetter}
-              onChange = {hadleInputLetter}
+              value={lastLetter}
+              onKeyDown={handleKeyDown}
+              onChange={handleChange}
             />
-            <button onClick={handlerError}>Incrementar</button>
           </form>
         </section>
-        <section className={`dummy error-${numberOfErrors}`}>
+        <section className={`dummy error-${getNumberOfErrors()}`}>
           <span className="error-13 eye"></span>
           <span className="error-12 eye"></span>
           <span className="error-11 line"></span>
           <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
+          <span className="error-9  line"></span>
+          <span className="error-8  line"></span>
+          <span className="error-7  line"></span>
+          <span className="error-6  head"></span>
+          <span className="error-5  line"></span>
+          <span className="error-4  line"></span>
+          <span className="error-3  line"></span>
+          <span className="error-2  line"></span>
+          <span className="error-1  line"></span>
         </section>
       </main>
     </div>
